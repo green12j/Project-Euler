bbcf8be73184b884837a50dbc3832a1464c2b00a
Jordan Greenfield <green12j@cmich.edu>
2016-09-06 23:31:45 -0400

    Rename Problem5 > Problem005

---
+++ ProjectEuler/src/Problem005.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author JGreeny
+ *
+ * @problem Project Euler Problem 5:
+ * 			2520 is the smallest number that can be divided by each of
+ * 	 		the numbers from 1 to 10 without any remainder.
+ * 			What is the smallest positive number that is evenly divisible
+ * 			by all of the numbers from 1 to 20?
+ *			
+ * @solution 
+ */
+
+public class Problem005 {
+	public static void main(String[] args) {
+		
+	}
+}
